01efe4bb340c49f9c966813f900ab6e38eef81ca
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-09-02 16:18:47 +0300

    #

---
+++ src/app/About/About.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    templateUrl: './About.html',
+    styleUrl: './About.css'
+}) export class About {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Privacy } from './privacy/privacy';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Privacy],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('AngularCourse');
+
+  helloWorld: string = "Hello world from TS";
+
+  clickMe() {
+    alert("Beni Tıkladın..")
+  }
+
+  changeHelloWorldVariable(event: any) {
+    this.helloWorld = event.target.value;
+  }
 }
