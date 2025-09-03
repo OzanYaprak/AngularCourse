853f9ad3ef23a08f99e8f3d30f54386a18c24a85
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-09-03 16:30:19 +0300

    #2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Privacy } from './privacy/privacy';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Privacy],
+  imports: [RouterOutlet, Privacy, NgClass, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,6 +13,11 @@ export class App {
   protected readonly title = signal('AngularCourse');
 
   helloWorld: string = "Hello world from TS";
+  pClassNameRed: string = "red";
+  pBackGroundColor: string = "backGroundColor";
+
+  bgColor = "red";
+
 
   clickMe() {
     alert("Beni Tıkladın..")
@@ -19,5 +25,9 @@ export class App {
 
   changeHelloWorldVariable(event: any) {
     this.helloWorld = event.target.value;
+  }
+
+  checkActive() {
+    return this.bgColor === 'red';
   }
 }
